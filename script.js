34c97424a8c85d9089fe656658c828a81b264043
khanh <phanmykhanh2k2@gmail.com>
2024-07-21 02:46:21 +0200

    please

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ menuButton.addEventListener("click", () => {
   menu.classList.toggle("navbar__open");
   menuButton.classList.toggle("open");
 });
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const serviceID = 'service_9nnnj9d';
+  const templateID = 'template_k2zm0hh';
+
+  emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+          alert('Message sent successfully!');
+          document.getElementById('myForm').reset();
+      }, (err) => {
+          alert('Failed to send message. Please try again.');
+          console.log('Failed to send email. Error:', err);
+      });
+});
 
 
